e7a1b6a6282971d9e788a1b77418f5556c4d435c
KinasGroup <info7admin@gmail.com>
2026-06-08 09:41:23 +0200

    Update login.php

--- auth/login.php
+++ auth/login.php
@@ -39,7 +39,7 @@ if ($registrationSuccess) {
     <aside class="je-auth-aside">
         <a href="../index.php" class="je-auth-brand">
             <img src="../assets/images/logos/kinas-group-logo.png" alt="KINAS GROUP" onerror="this.style.display='none'">
-            <span>KINAS GROUP</span>
+            <span></span>
         </a>
         <div>
             <h1 class="je-auth-headline">The World's Luxury Marketplace.</h1>
