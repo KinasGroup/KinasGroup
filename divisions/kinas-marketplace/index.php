43fa419684771e65551f02bae437e74335928fa4
KinasGroup <info7admin@gmail.com>
2026-06-14 18:01:23 +0200

    Update index.php

--- divisions/kinas-marketplace/index.php
+++ divisions/kinas-marketplace/index.php
@@ -227,10 +227,10 @@ include '../../templates/header.php';
 <section id="heroSection" style="position:relative; height:70vh; min-height:480px; padding-top:90px; box-sizing:border-box; display:flex; align-items:center; overflow:hidden;">
     <!-- Rotating Background Slides -->
     <div class="hero-slides">
-        <div class="hero-slide active" style="background-image: url('https://images.unsplash.com/photo-1490481651871-ab68de25d43d?w=1920&q=80'); background-position: center 35%;"></div>
-        <div class="hero-slide" style="background-image: url('https://images.unsplash.com/photo-1523275335684-37898b6baf30?w=1920&q=80'); background-position: center 30%;"></div>
-        <div class="hero-slide" style="background-image: url('https://images.unsplash.com/photo-1585386959984-a4155221c2c9?w=1920&q=80'); background-position: center 25%;"></div>
-        <div class="hero-slide" style="background-image: url('https://images.unsplash.com/photo-1537832816519-689ad163238b?w=1920&q=80'); background-position: center 40%;"></div>
+        <div class="hero-slide active" style="background-image: url('https://images.pexels.com/photos/298863/pexels-photo-298863.jpeg?w=1920&q=80'); background-position: center 35%;"></div>
+        <div class="hero-slide" style="background-image: url('https://images.pexels.com/photos/190819/pexels-photo-190819.jpeg?w=1920&q=80'); background-position: center 30%;"></div>
+        <div class="hero-slide" style="background-image: url('https://images.pexels.com/photos/965989/pexels-photo-965989.jpeg?w=1920&q=80'); background-position: center 25%;"></div>
+        <div class="hero-slide" style="background-image: url('https://images.pexels.com/photos/437037/pexels-photo-437037.jpeg?w=1920&q=80'); background-position: center 40%;"></div>
     </div>
     <div class="hero-overlay"></div>
     
