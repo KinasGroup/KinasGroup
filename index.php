7f70ec1cbdd48315d7f1caee53f9da29c8d23039
KinasGroup <info7admin@gmail.com>
2026-06-25 03:53:21 +0200

    Update index.php

--- index.php
+++ index.php
@@ -524,7 +524,7 @@ include 'templates/header.php';
             
             <!-- Verified Sellers -->
             <div class="feature-card">
-                <div class="feature-bg" style="background-image: url('https://images.pexels.com/photos/3862624/pexels-photo-3862624.jpeg?w=600&q=80'); background-color: #1a2e1a;"></div>
+                <div class="feature-bg" style="background-image: url('https://images.pexels.com/photos/6199250/pexels-photo-6199250.jpeg?w=600&q=80'); background-color: #1a2e1a;"></div>
                 <div class="feature-overlay"></div>
                 <div class="feature-content">
                     <h3>Verified Sellers</h3>
@@ -534,7 +534,7 @@ include 'templates/header.php';
             
             <!-- Global Reach -->
             <div class="feature-card">
-                <div class="feature-bg" style="background-image: url('https://images.pexels.com/photos/31134828/pexels-photo-31134828.jpeg?w=600&q=80'); background-color: #0c1a2e;"></div>
+                <div class="feature-bg" style="background-image: url('https://images.pexels.com/photos/393570/pexels-photo-393570.jpeg?w=600&q=80'); background-color: #0c1a2e;"></div>
                 <div class="feature-overlay"></div>
                 <div class="feature-content">
                     <h3>Global Reach</h3>
