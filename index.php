a7bf15c87339fcc28e29643f7635a3680079bea2
KinasGroup <info7admin@gmail.com>
2026-06-26 19:05:58 +0200

    Update index.php

--- index.php
+++ index.php
@@ -559,7 +559,7 @@ include 'templates/header.php';
             
             <!-- KINAS Marketplace -->
             <a href="/divisions/kinas-marketplace/" class="division-card">
-                <div class="card-bg" style="background-image: url('https://images.unsplash.com/photo-1558002038-1055907df827?w=600&q=80');"></div>
+                <div class="card-bg" style="background-image: url('https://images.unsplash.com/photo-1557324232-b8917d3c3dcb?w=600&q=80');"></div>
                 <div class="card-overlay"></div>
                 <div class="card-content">
                     <h3>KINAS Marketplace</h3>
