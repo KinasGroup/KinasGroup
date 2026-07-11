f8a4e92f9b80521819b1aab3ae8c966d309a1819
KinasGroup <info7admin@gmail.com>
2026-07-11 16:06:11 +0100

    Update verify-phone.php

--- auth/verify-phone.php
+++ auth/verify-phone.php
@@ -250,7 +250,7 @@ $appEnv = getenv('APP_ENV') ?: ($_ENV['APP_ENV'] ?? '');
     <aside class="je-auth-aside">
         <a href="../index.php" class="je-auth-brand">
             <img src="../assets/images/logos/kinas-group-logo.png" alt="KINAS GROUP" onerror="this.style.display='none'">
-            <span>KINAS GROUP</span>
+            <span></span>
         </a>
         <div>
             <h1 class="je-auth-headline">One quick step.</h1>
