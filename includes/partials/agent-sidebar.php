2db8fd2f707b3a0d4a03afb2a95edde8bd32ffa8
KinasGroup <info7admin@gmail.com>
2026-08-16 15:01:57 +0100

    Update agent-sidebar.php

--- includes/partials/agent-sidebar.php
+++ includes/partials/agent-sidebar.php
@@ -1,5 +1,6 @@
 <?php
 require_once __DIR__ . '/../../includes/je-sidebar.php';
+require_once __DIR__ . '/../../includes/public-identity.php';
 $currentPage = basename($_SERVER['PHP_SELF']);
 je_render_sidebar('agent', $currentPage, 2);
 ?>
